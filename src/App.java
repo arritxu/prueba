317ca3974cf2c4f3a05666b622efae47cbcf56ca
Arritxu <you@example.com>
2023-12-26 22:51:42 +0100

    commit otro

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Rama2!!");
+        System.out.println("Hello Repositorio Remoto");
     }
 }
