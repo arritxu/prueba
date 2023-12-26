5adb92a41d92871bfece3351d38bee405ffba2f7
Arritxu <you@example.com>
2023-12-26 23:14:21 +0100

    commit dos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Repositorio Remoto ");
+        System.out.println("Hello Repositorio Remoto");
     }
 }
